ca8c2abaafc09e2319eec9b8e2b99a7ce4019397
shreyabhosalee <shreyabhosalee@gmail.com>
2026-09-25 14:10:40 +0000

    hello

---
+++ Student.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Student {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+
+        System.out.print("Enter your marks: ");
+        float marks = sc.nextFloat();
+
+        System.out.println("\n--- Student Details ---");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Marks: " + marks);
+
+        sc.close();
+    }
+}
